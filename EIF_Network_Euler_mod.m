350771309e6f115e203718118bbc94ea3e98b177
alexmonteil <alex.monteil@outlook.com>
2025-10-02 21:00:55 -0500

    Added the files for the modified simulation
    Files for modified simulation for team mini project

---
+++ EIF_Network_Euler_mod.m
@@ -0,0 +1,46 @@
+function [Se, Si] = EIF_Network_Euler_mod(Ne,Ni,VT,EL,time,dt,D, taum, taux, ...
+    taue, taui, Vth, Vre, Sex, Six,Jex,Jee,Jei,Jix,Jie,Jii)
+
+
+% Initialize membrane potentials randomly
+Ve=rand(Ne,1)*(VT-EL)+EL;
+Vi=rand(Ni,1)*(VT-EL)+EL;
+
+% Initialize all else as zeros
+Iex=zeros(Ne,1);
+Iee=zeros(Ne,1);
+Iei=zeros(Ne,1);
+Iix=zeros(Ni,1);
+Iie=zeros(Ni,1);
+Iii=zeros(Ni,1);
+Se=zeros(Ne,length(time));
+Si=zeros(Ni,length(time));
+
+for tt = 1:length(time)-1
+    % Euler step for membrane potentials
+    Ve=Ve+dt*(-(Ve-EL)+D*exp((Ve-VT)/D)+Iex+Iee+Iei)/taum; 
+    Vi=Vi+dt*(-(Vi-EL)+D*exp((Vi-VT)/D)+Iix+Iie+Iii)/taum; 
+    
+    % Euler step for synaptic currents
+    Iex=Iex+dt*(-Iex+Jex*Sex(:,tt))/taux;      
+    Iee=Iee+dt*(-Iee+Jee*Se(:,tt))/taue;
+    Iei=Iei+dt*(-Iei+Jei*Si(:,tt))/taui;   
+    Iix=Iix+dt*(-Iix+Jix*Six(:,tt))/taux;    
+    Iie=Iie+dt*(-Iie+Jie*Se(:,tt))/taue;
+    Iii=Iii+dt*(-Iii+Jii*Si(:,tt))/taui;
+    
+        
+    % Find which excitatory neurons spiked.
+    Inds= Ve>=Vth;
+    % Reset membrane potentials
+    Ve(Inds)=Vre;    
+    % Store spikes as delta functions
+    Se(Inds,tt+1)=1/dt;
+        
+    % Now do the same for inhibitory neurons
+    Inds= Vi>=Vth;
+    % Reset membrane potentials
+    Vi(Inds)=Vre;
+    % Store spikes as delta functions
+    Si(Inds,tt+1)=1/dt;
+end
